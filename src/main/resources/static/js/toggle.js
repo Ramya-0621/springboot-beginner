2fbe7a4e2f2cc529871c9e74a356dd547434e435
Ramya Vura <vuraramya@gmail.com>
2026-05-27 23:22:59 +0530

    toggle of cards in different pages

--- src/main/resources/static/js/toggle.js
+++ src/main/resources/static/js/toggle.js
@@ -1,31 +1,22 @@
 var users = [
-
     {
-        name : "Joe",
-        gender : "Male",
-        img : "https://www.w3schools.com/howto/img_avatar.png"
+        name: "Joe",
+        gender: "Male",
+        img: "https://www.w3schools.com/HOWTO/img_avatar.png"
     },
-
     {
-        name : "Jane",
-        gender : "Female",
-        img : "https://www.w3schools.com/w3images/avatar2.png"
+        name: "Jane Doe",
+        gender: "Female",
+        img: "https://www.w3schools.com/w3images/avatar2.png"
     }
-
 ];
 
 var currentUser = 0;
 
-function toggleCard(){
+function toggleUser() {
+    currentUser = (currentUser === 0) ? 1 : 0;
 
-    currentUser = (currentUser + 1) % users.length;
-
-    document.getElementById("userName").innerHTML =
-    users[currentUser].name;
-
-    document.getElementById("userGender").innerHTML =
-    users[currentUser].gender;
-
-    document.getElementById("userImage").src =
-    users[currentUser].img;
+    document.getElementById("userName").innerText = users[currentUser].name;
+    document.getElementById("userGender").innerText = users[currentUser].gender;
+    document.getElementById("userImage").src = users[currentUser].img;
 }
