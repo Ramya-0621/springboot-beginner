5779f83f7d03b532d66b0b6f5489addd5405afcd
Ramya Vura <vuraramya@gmail.com>
2026-05-28 15:00:39 +0530

    toggle card with java script

---
+++ src/main/resources/static/js/toggle.js
@@ -0,0 +1,20 @@
+var currentUserIndex = 1;
+var userArray = [
+    { name: "John Doe", gender: "Male", img: "https://www.w3schools.com/HOWTO/img_avatar.png" },
+    { name: "Jane Doe", gender: "Female", img: "https://www.w3schools.com/HOWTO/img_avatar2.png" }
+];
+
+function toggleUser() {
+    // 1. Flip the index
+    currentUserIndex = 1 - currentUserIndex;
+    var nextuser = userArray[currentUserIndex];
+    
+    // 2. Call the display function to actually update the screen
+    displayUser(nextuser);
+}
+
+function displayUser(user) {
+    document.querySelector("img").src = user.img;
+    document.querySelector("h3").textContent = user.name;
+    document.querySelectorAll("h3")[1].textContent = user.gender; 
+}
